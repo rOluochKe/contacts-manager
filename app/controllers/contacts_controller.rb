f8ca5184dcf419b1d4bcd7778ed9f5dfb8dd21cc
roluochKe <rasholuoch@gmail.com>
2021-11-23 14:24:40 +0300

    working with data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,9 +3,45 @@ class ContactsController < ApplicationController
     if params[:group_id] && !params[:group_id].empty?
       # @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
 
-      @contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+      @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
     else
-      @contacts = Contact.page(params[:page])
+      @contacts = Contact.order(created_at: :desc).page(params[:page])
     end
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+        flash[:success] = "Contact was successfully created."
+        redirect_to contacts_path
+    else
+        render 'new'
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    
+    if @contact.update(contact_params)
+      flash[:success] = "Contact was successfully updated."
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
   end
 end
