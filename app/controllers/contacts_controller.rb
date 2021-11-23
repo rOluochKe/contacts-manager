f516183a2b381f8116dbd1357fd9c41d8684787f
roluochKe <rasholuoch@gmail.com>
2021-11-23 12:47:52 +0300

    presenting data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,11 @@
 class ContactsController < ApplicationController
   def index
+    if params[:group_id] && !params[:group_id].empty?
+      # @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
+
+      @contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+    else
+      @contacts = Contact.page(params[:page])
+    end
   end
 end
